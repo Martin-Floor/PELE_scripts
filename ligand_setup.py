14ab921b660c181ea3d3b8453500b52ce9d615ef
Martin-Floor <martin.floor@uvic.cat>
2021-12-16 17:00:19 +0100

    Skipped flags working

--- ligand_setup.py
+++ ligand_setup.py
@@ -14,14 +14,17 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('input_file', help='Input PDB file contatining ligand.')
 parser.add_argument('ligand_name', default="UNK", help='Three letter code of the PDB ligand name.')
-parser.add_argument('--max_min_steps', default=100, help='Maximum ligand minimization steps.')
+parser.add_argument('--max_min_steps', default=1000, help='Maximum ligand minimization steps.')
 parser.add_argument('--n_conformers', default=10, help='Number of conformers to generate.')
 parser.add_argument('--functional', default='B3LYP-D3', help='DFT functional to use for conformer optimization (Jaguar).')
 parser.add_argument('--basis_set', default='CC-PVTZ', help='DFT basis set to use for conformer optimization (Jaguar).')
 parser.add_argument('--cpus', default=40, help='Number of CPUs to use in conformer optimization (Jaguar).')
-parser.add_argument('--skip_conformers', default=False, action='store_false', help='Debug option: skip conformers generation step')
-parser.add_argument('--skip_optimization', default=False, action='store_false', help='Debug option: skip optimization step')
-parser.add_argument('--skip_resp', default=False, action='store_false', help='Debug option: skip resp fitting step')
+parser.add_argument('--skip_minimization', default=False, action='store_true', help='Debug option: skip ligand minimization step')
+parser.add_argument('--skip_conformers', default=False, action='store_true', help='Debug option: skip conformers generation step')
+parser.add_argument('--skip_optimization', default=False, action='store_true', help='Debug option: skip optimization step')
+parser.add_argument('--skip_resp', default=False, action='store_true', help='Debug option: skip resp fitting step')
+parser.add_argument('--min_conv_threshold', default=0.0001, help='Threshold to stop ligand minimization before total number of steps are reached.')
+parser.add_argument('--min_step_report', default=10, help='Number of steps to report the change in minimized energy.')
 
 args=parser.parse_args()
 
@@ -32,11 +35,19 @@ n_conformers = int(args.n_conformers)
 functional = args.functional
 basis_set = args.basis_set
 cpus = int(args.cpus)
+min_conv_threshold = float(args.min_conv_threshold)
+min_step_report = int(args.min_step_report)
 
 # Debug variables
+skip_minimization = args.skip_minimization
 skip_conformers = args.skip_conformers
 skip_optimization = args.skip_optimization
 skip_resp = args.skip_resp
+
+if skip_minimization:
+    run_minimization = False
+else:
+    run_minimization = True
 
 if skip_conformers:
     run_conformers = False
@@ -53,6 +64,11 @@ if skip_resp:
 else:
     run_resp = True
 
+# Define folders
+conformer_folder = '01_conformers'
+optimization_folder = '02_optimization'
+resp_folder = '03_resp_charges'
+
 ### Read input ligand ###
 print('Reading ligand file: %s' % input_file)
 for st in structure.StructureReader(input_file):
@@ -60,51 +76,58 @@ for st in structure.StructureReader(input_file):
     break
 
 ### Ligand minimization ###
-
-# Set the energy property name
-print('Minimizing ligand')
-energy_name = 'r_ff_Potential_Energy-OPLS_2005'
-# Constants
-OPLS_2005 = 14
-minimize_structure(st, ffld_version=OPLS_2005, max_steps=0)
-for i in range(max_min_steps):
+if run_minimization:
+    # Set the energy property name
+    print('Minimizing ligand')
+    energy_name = 'r_ff_Potential_Energy-OPLS_2005'
+    # Constants
+    OPLS_2005 = 14
+    minimize_structure(st, ffld_version=OPLS_2005, max_steps=0)
     initial_energy = st.property[energy_name]
-    minimize_structure(st, ffld_version=OPLS_2005, max_steps=1)
-    minimized_energy = st.property[energy_name]
-    delta_energy = initial_energy - minimized_energy
-    print("The minimized energy is {} kcal/mol lower than the original for step {}.".format(
-        delta_energy, i+1))
-    if delta_energy == 0.0:
-        print('Minimization finished.')
-        break
+    for i in range(max_min_steps):
+        minimize_structure(st, ffld_version=OPLS_2005, max_steps=1)
+        if i % min_step_report == 0:
+            minimized_energy = st.property[energy_name]
+            delta_energy = initial_energy - minimized_energy
+            print("The minimized delta energy is {} kcal/mol for step {}.".format(delta_energy, i+1))
+            initial_energy = st.property[energy_name]
+        if delta_energy <= min_conv_threshold:
+            print('Minimization finished.')
+            break
 
-### Conformer search ###
-print('Calculating ligand conformers')
-
-conformer_folder = '01_conformers'
-if not os.path.exists(conformer_folder):
-    os.mkdir(conformer_folder)
-
-# Save structure
-minimized_file = 'ligand_minimized.mae'
-st.write(conformer_folder+'/'+minimized_file)
-
-os.chdir(conformer_folder)
-command = '${SCHRODINGER}/confgenx '
-command += minimized_file+' '
-command += '-jobname confgen_1 '
-command += '-max_num_conformers '+str(n_conformers)+' '
-command += ' -optimize -HOST localhost'
+    # Save structure
+    minimized_file = 'ligand_minimized.mae'
+    st.write(minimized_file)
 
 if run_conformers:
+
+    ### Conformer search ###
+    print('Calculating ligand conformers')
+
+    if not os.path.exists(conformer_folder):
+        os.mkdir(conformer_folder)
+
+    # Save structure
+    minimized_file = '../ligand_minimized.mae'
+
+    os.chdir(conformer_folder)
+    command = '${SCHRODINGER}/confgenx '
+    command += minimized_file+' '
+    command += '-jobname confgen_1 '
+    command += '-max_num_conformers '+str(n_conformers)+' '
+    command += ' -optimize -HOST localhost'
+
     p = subprocess.Popen(command, shell=True)
     # Wait until conformers file is generated
     while not os.path.exists('confgen_1-out.maegz'):
         time.sleep(1)
 
+    print('Finished conformer generation.')
+    os.chdir('..')
+
 # Get the number of conformers generated
 generated_conformers = 0
-with open('confgen_1.log') as cgl:
+with open(conformer_folder+'/confgen_1.log') as cgl:
     for l in cgl:
         if 'structures stored' in l:
             generated_conformers = int(l.split()[0])
@@ -112,102 +135,100 @@ with open('confgen_1.log') as cgl:
 if generated_conformers < n_conformers:
     print('Only %s of %s conformers were generated!' % (generated_conformers, n_conformers))
 
-print('Finished conformer generation.')
-os.chdir('..')
-
-### Set up Jaguar calculations ###
-print('DFT optimization')
-optimization_folder = '02_optimization'
-if not os.path.exists(optimization_folder):
-    os.mkdir(optimization_folder)
-os.chdir(optimization_folder)
-
-command = '${SCHRODINGER}/jaguar run canonical.py '
-command += '-jobname=jag_batch_opt_'+functional+'_'+basis_set+' '
-command += '-keyword=isolv=2 '
-command += '-keyword=basis='+basis_set+' '
-command += '-keyword=ip172=2 '
-command += '-keyword=gcharge=-2 '
-command += '-keyword=igeopt=1 '
-command += '-keyword=icfit=1 '
-command += '-keyword=dftname='+functional+' '
-command += '-keyword=nogas=0 '
-command += '../'+conformer_folder+'/confgen_1-out.maegz '
-command += '-HOST localhost '
-command += '-PARALLEL '+str(cpus)+' '
-command += '-TMPLAUNCHDIR'
-
 if run_optimization:
+    ### Set up Jaguar calculations ###
+    print('DFT optimization')
+    if not os.path.exists(optimization_folder):
+        os.mkdir(optimization_folder)
+    os.chdir(optimization_folder)
+
+    command = '${SCHRODINGER}/jaguar run canonical.py '
+    command += '-jobname=jag_batch_opt_'+functional+'_'+basis_set+' '
+    command += '-keyword=isolv=2 '
+    command += '-keyword=basis='+basis_set+' '
+    command += '-keyword=ip172=2 '
+    command += '-keyword=gcharge=-2 '
+    command += '-keyword=igeopt=1 '
+    command += '-keyword=icfit=1 '
+    command += '-keyword=dftname='+functional+' '
+    command += '-keyword=nogas=0 '
+    command += '../'+conformer_folder+'/confgen_1-out.maegz '
+    command += '-HOST localhost '
+    command += '-PARALLEL '+str(cpus)+' '
+    command += '-TMPLAUNCHDIR'
+
     p = subprocess.Popen(command, shell=True)
 
-# Check until all optimizations are finished
-log_files = {}
-# Get log file paths
-for f in os.listdir():
-    if 'isomer' in f and f.endswith('.log'):
-        index = int(f.split('isomer')[-1].split('.')[0])
-        log_files[index] = f
+    # Check until all optimizations are finished
+    log_files = {}
+    # Get log file paths
+    for f in os.listdir():
+        if 'isomer' in f and f.endswith('.log'):
+            index = int(f.split('isomer')[-1].split('.')[0])
+            log_files[index] = f
 
-finished = []
-while False in finished or len(finished) < generated_conformers:
     finished = []
-    for isomer in log_files:
-        ended = False
-        with open(log_files[isomer]) as isof:
-            for l in isof:
-                if 'Finished' in l:
-                    ended = True
-        if ended:
-            finished.append(True)
-        else:
-            finished.append(False)
-    time.sleep(3)
+    while False in finished or len(finished) < generated_conformers:
+        finished = []
+        for isomer in log_files:
+            ended = False
+            with open(log_files[isomer]) as isof:
+                for l in isof:
+                    if 'Finished' in l:
+                        ended = True
+            if ended:
+                finished.append(True)
+            else:
+                finished.append(False)
+        time.sleep(3)
 
-print('Finished DFT optimization.')
-os.chdir('..')
+    print('Finished DFT optimization.')
+    os.chdir('..')
+
+if os.path.exists(optimization_folder):
+    # Read log files
+    log_files = {}
+    # Get log file paths
+    for f in os.listdir(optimization_folder):
+        if 'isomer' in f and f.endswith('.log'):
+            index = int(f.split('isomer')[-1].split('.')[0])
+            log_files[index] = '../'+optimization_folder+'/'+f
+else:
+    if run_resp:
+        print('Optimization folder not found. Cannot calculate RESP')
+        exit()
 
 # Generate RESP charges
+def readMol2Charges(mol2_file):
+    """
+    Read and return charges from a mol2 file
 
-print('Calculating multi-configuration RESP charges')
-resp_folder = '03_resp_charges'
-if not os.path.exists(resp_folder):
-    os.mkdir(resp_folder)
-os.chdir(resp_folder)
+    Parameters
+    ==========
+    mol2_file : str
+        Path to a mol2 file
 
-# Create MultiConfiguration resp file for antechamber
-resp_files = {}
-with open('MC.resp', 'w') as iso:
-    for f in os.listdir('../'+optimization_folder):
-        if 'isomer' in f and f.endswith('.resp'):
-            with open('../'+optimization_folder+'/'+f) as isoi:
-                for i,l in enumerate(isoi):
-                    if i == 0:
-                        ls = l.split()
-                        iso.write('%4s %-8s\n' % (ls[0], ls[1]))
-                    else:
-                        iso.write(l)
+    Returns
+    =======
+    charges : dict
+        Dictionary containing the atom names as keys and charges as values.
+    """
+    charges = {}
+    with open(mol2_file) as mf:
+        c = False
+        for l in mf:
+            if '@<TRIPOS>ATOM' in l:
+                c = True
+                continue
+            elif '@<TRIPOS>BOND' in l:
+                c = False
+                continue
+            if c:
+                charge = float(l.strip().split()[-1])
+                atom = l.strip().split()[1]
+                charges[atom] = charge
 
-# Create mol2 for fitting
-for st in structure.StructureReader('../'+input_file):
-    st.write(ligand_name+'.mol2', format='mol2')
-
-## Read optimization energies
-optimization_energies = {}
-for isomer in sorted(log_files):
-    optimization_energies[isomer] = []
-    with open('../'+optimization_folder+'/'+log_files[isomer]) as isof:
-        for l in isof:
-            if 'Total energy' in l:
-                optimization_energies[isomer].append(float(l.split()[2]))
-
-# Calcualte Boltzmann probabilities
-KT = 0.593 # kcal/mol
-hartree_to_kcal_mol = 627.503
-final_energies = [optimization_energies[isomer][-1] for isomer in sorted(log_files)]
-final_energies = np.array(final_energies)*hartree_to_kcal_mol # Convert to kcal/mol
-relative_energies = final_energies - np.min(final_energies) # relative energies
-partition_coefficient = np.sum(np.exp(-relative_energies/KT))
-probabilities = np.exp(-relative_energies/KT)/partition_coefficient
+    return charges
 
 # Calculate MC RESP charges
 def changeRespinWeight(respin_file, weights):
@@ -242,61 +263,72 @@ def changeRespinWeight(respin_file, weights):
                     trif.write(l)
     shutil.move(respin_file+'.tmp', respin_file)
 
-command = 'antechamber -fi mol2 -fo ac -i '+ligand_name+'.mol2'+' -o '+ligand_name+'.ac\n'
-command += 'respgen -i '+ligand_name+'.ac -o '+ligand_name+'.respin1 -f resp1 -n '+str(generated_conformers)+'\n'
-command += 'respgen -i '+ligand_name+'.ac -o '+ligand_name+'.respin2 -f resp2 -n '+str(generated_conformers)
+
 if run_resp:
+
+    print('Calculating multi-configuration RESP charges')
+    if not os.path.exists(resp_folder):
+        os.mkdir(resp_folder)
+    os.chdir(resp_folder)
+
+    # Create MultiConfiguration resp file for antechamber
+    resp_files = {}
+    with open('MC.resp', 'w') as iso:
+        for f in os.listdir('../'+optimization_folder):
+            if 'isomer' in f and f.endswith('.resp'):
+                with open('../'+optimization_folder+'/'+f) as isoi:
+                    for i,l in enumerate(isoi):
+                        if i == 0:
+                            ls = l.split()
+                            iso.write('%4s %-8s\n' % (ls[0], ls[1]))
+                        else:
+                            iso.write(l)
+
+    # Create mol2 for fitting
+    for f in os.listdir('../'+optimization_folder):
+        if f.endswith('.mae') and 'isomer1.01' in f:
+            for st in structure.StructureReader('../'+optimization_folder+'/'+f):
+                st.write(ligand_name+'.mol2', format='mol2')
+
+    ## Read optimization energies
+    optimization_energies = {}
+    for isomer in sorted(log_files):
+        optimization_energies[isomer] = []
+        with open(log_files[isomer]) as isof:
+            for l in isof:
+                if 'Total energy' in l:
+                    optimization_energies[isomer].append(float(l.split()[2]))
+
+    # Calcualte Boltzmann probabilities
+    KT = 0.593 # kcal/mol
+    hartree_to_kcal_mol = 627.503
+    final_energies = [optimization_energies[isomer][-1] for isomer in sorted(log_files)]
+    final_energies = np.array(final_energies)*hartree_to_kcal_mol # Convert to kcal/mol
+    relative_energies = final_energies - np.min(final_energies) # relative energies
+    partition_coefficient = np.sum(np.exp(-relative_energies/KT))
+    probabilities = np.exp(-relative_energies/KT)/partition_coefficient
+
+    command = 'antechamber -fi mol2 -fo ac -i '+ligand_name+'.mol2'+' -o '+ligand_name+'.ac\n'
+    command += 'respgen -i '+ligand_name+'.ac -o '+ligand_name+'.respin1 -f resp1 -n '+str(generated_conformers)+'\n'
+    command += 'respgen -i '+ligand_name+'.ac -o '+ligand_name+'.respin2 -f resp2 -n '+str(generated_conformers)
     subprocess.call(command, shell=True)
-changeRespinWeight(ligand_name+'.respin1', probabilities)
-changeRespinWeight(ligand_name+'.respin2', probabilities)
-command = 'resp -O -i '+ligand_name+'.respin1 -o '+ligand_name+'.respout1 -e MC.resp -t '+ligand_name+'_qout_stage1\n'
-command += 'resp -O -i '+ligand_name+'.respin2 -o '+ligand_name+'.respout2 -e MC.resp -q '+ligand_name+'_qout_stage1 -t '+ligand_name+'_qout_stage2\n'
-command += 'antechamber -i '+ligand_name+'.mol2 -fi mol2 -o '+ligand_name+'_resp.mol2 -fo mol2 -c rc -cf '+ligand_name+'_qout_stage2\n'
-if run_resp:
+    changeRespinWeight(ligand_name+'.respin1', probabilities)
+    changeRespinWeight(ligand_name+'.respin2', probabilities)
+    command = 'resp -O -i '+ligand_name+'.respin1 -o '+ligand_name+'.respout1 -e MC.resp -t '+ligand_name+'_qout_stage1\n'
+    command += 'resp -O -i '+ligand_name+'.respin2 -o '+ligand_name+'.respout2 -e MC.resp -q '+ligand_name+'_qout_stage1 -t '+ligand_name+'_qout_stage2\n'
+    command += 'antechamber -i '+ligand_name+'.mol2 -fi mol2 -o '+ligand_name+'_resp.mol2 -fo mol2 -c rc -cf '+ligand_name+'_qout_stage2\n'
     subprocess.call(command, shell=True)
-os.chdir('..')
-print('RESP fitting finished.')
+    os.chdir('..')
+    print('RESP fitting finished.')
 
-# Create Maestro file with RESP charges
-def readMol2Charges(mol2_file):
-    """
-    Read and return charges from a mol2 file
+    # Assign residue name
+    for residue in st.residue:
+        residue.pdbres = ligand_name
 
-    Parameters
-    ==========
-    mol2_file : str
-        Path to a mol2 file
+    charges = readMol2Charges(resp_folder+'/'+ligand_name+'_resp.mol2')
 
-    Returns
-    =======
-    charges : dict
-        Dictionary containing the atom names as keys and charges as values.
-    """
-    charges = {}
-    with open(mol2_file) as mf:
-        c = False
-        for l in mf:
-            if '@<TRIPOS>ATOM' in l:
-                c = True
-                continue
-            elif '@<TRIPOS>BOND' in l:
-                c = False
-                continue
-            if c:
-                charge = float(l.strip().split()[-1])
-                atom = l.strip().split()[1]
-                charges[atom] = charge
+    for atom in st.atom:
+        atom_name = atom.pdbname.strip()
+        atom.partial_charge = charges[atom_name]
 
-    return charges
-
-# Assign residue name
-for residue in st.residue:
-    residue.pdbres = ligand_name
-
-charges = readMol2Charges(resp_folder+'/'+ligand_name+'_resp.mol2')
-
-for atom in st.atom:
-    atom_name = atom.pdbname.strip()
-    atom.partial_charge = charges[atom_name]
-
-st.write(resp_folder+'/'+ligand_name+'_resp.mae')
+    st.write(resp_folder+'/'+ligand_name+'_resp.mae')
