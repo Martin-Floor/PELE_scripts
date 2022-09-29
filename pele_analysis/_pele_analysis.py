6f63d73cd0becfeb66f074a58b75c4a222ca003e
mluengo <miguelluengo.bcn@gmail.com>
2022-09-29 11:36:31 +0200

    Fixed bug when no equilibration files are found.

--- pele_analysis/_pele_analysis.py
+++ pele_analysis/_pele_analysis.py
@@ -2653,7 +2653,7 @@ class peleAnalysis:
                 else:
                     report_files = reports_dict[protein][ligand]
 
-            if report_files == None and equilibration and self.csv_equilibration_files == {}:
+            if report_files == {} and equilibration and self.csv_equilibration_files == {}:
                 print('WARNING: No equilibration data found for simulation %s-%s' % (protein, ligand))
                 continue
 
@@ -2688,6 +2688,10 @@ class peleAnalysis:
             report_data.append(data)
             if verbose:
                 print('\t in %.2f seconds.' % (time.time()-start))
+
+        if report_data == [] and equilibration:
+            self.equilibration_data = None
+            return
 
         # Merge all dataframes into a single dataframe
         self.data = pd.concat(report_data)
