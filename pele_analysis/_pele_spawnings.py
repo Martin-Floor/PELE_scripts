c210940fe56ba30680bf4609e99c572fcc6b7ce8
Martin-Floor <martinfloor@gmail.com>
2024-10-08 06:47:30 +0200

    Improved formatPELESpawnings

--- pele_analysis/_pele_spawnings.py
+++ pele_analysis/_pele_spawnings.py
@@ -2,11 +2,30 @@ import os
 import shutil
 import json
 
-def formatPELESpawnings(pele_folder, separator='-'):
+def formatPELESpawnings(pele_folder, separator='-', proteins=None, ligands=None, combinations=None, keep_original=False, output_folder=None,
+                        skip_0_epochs=True):
     """
     Format the spawning PELE folder with the standard PELE format for easier analysis
     with peleAnalysis.
     """
+
+    if keep_original and not output_folder:
+        raise ValueError('You must provide an output folder if you want to preserve the original folder.')
+
+    elif output_folder and not keep_original:
+        keep_original = True
+
+    if proteins and isinstance(proteins, str):
+        proteins = [proteins]
+
+    if ligands and isinstance(ligands, str):
+        ligands = [ligands]
+
+    if combinations and isinstance(combinations, tuple):
+        combinations = [combinations]
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     for pele in os.listdir(pele_folder):
 
@@ -22,11 +41,20 @@ def formatPELESpawnings(pele_folder, separator='-'):
 
         # Get protein and ligand name
         protein, ligand = pele.split(separator)
-        pele = pele_folder+'/'+pele
+        pele_path = pele_folder+'/'+pele
+
+        if proteins and protein not in proteins:
+            continue
+
+        if ligands and ligand not in ligands:
+            continue
+
+        if combinations and (protein, ligand) not in combinations:
+            continue
 
         # Get spawnings if found
         spawnings = []
-        for spawning in os.listdir(pele):
+        for spawning in os.listdir(pele_path):
             try:
                 spawnings.append(int(spawning))
             except:
@@ -35,34 +63,47 @@ def formatPELESpawnings(pele_folder, separator='-'):
         # Check if the given folder has the spawning format
         if not spawnings:
 
-            if os.path.exists(pele+'/._spawning_mapping.json'):
-                print(f'PELE folder {pele} was already converted.')
+            if os.path.exists(pele_path+'/._spawning_mapping.json'):
+                print(f'PELE folder {pele_path} was already converted.')
             else:
-                print(f'PELE folder {pele} has not a spawning format. Skiping {protein} and {ligand}.')
+                print(f'PELE folder {pele_path} has not a spawning format. Skiping {protein} and {ligand}.')
             continue
 
         spawnings = sorted(spawnings)
 
         if 0 not in spawnings:
-            raise ValueError(f'First spawning folder was not found at {pele}. Please check your folder!')
+            raise ValueError(f'First spawning folder was not found at {pele_path}. Please check your folder for {protein} and {ligand}!')
+
+        if not os.path.exists(pele_path+'/0/output/output'):
+            print(f'There is no output folder for the first spawning folder. Skipping {protein} and {ligand}.')
+            continue
 
         # Copy full spawning-zero folder to tmp
-        tmp = pele+'_tmp'
-        shutil.copytree(pele+'/0', tmp, symlinks=True)
+        if keep_original:
+            tmp = output_folder+'/'+pele
+        else:
+            tmp = pele_path+'_tmp'
+        shutil.copytree(pele_path+'/0', tmp, symlinks=True)
 
         # Copy metrics dictionaries
-        if os.path.exists(pele+'/metrics.json'):
-            shutil.copy(pele+'/metrics.json', tmp+'/metrics.json')
-        if os.path.exists(pele+'/metrics_thresholds.json'):
-            shutil.copy(pele+'/metrics_thresholds.json', tmp+'/metrics_thresholds.json')
+        if os.path.exists(pele_path+'/metrics.json'):
+            shutil.copy(pele_path+'/metrics.json', tmp+'/metrics.json')
+        if os.path.exists(pele_path+'/metrics_thresholds.json'):
+            shutil.copy(pele_path+'/metrics_thresholds.json', tmp+'/metrics_thresholds.json')
 
-        # Get latest epoch at first spawning
+        # Get epochs at first spawning
         epochs = []
         for epoch in os.listdir(tmp+'/output/output'):
             try:
                 epochs.append(int(epoch))
             except:
                 continue
+
+        if not epochs:
+            print(f'There are no epochs for the first spawning folder. Skipping {protein} and {ligand}.')
+            shutil.rmtree(tmp)
+            continue
+
         epochs = sorted(epochs)
 
         current_epoch = epochs[-1] # Last epoch from first spawning
@@ -76,18 +117,29 @@ def formatPELESpawnings(pele_folder, separator='-'):
             if spawning == 0:
                 continue
 
+            if not os.path.exists(pele_path+'/'+str(spawning)+'/output/output'):
+                print(f'There is no output folder for spawning {str(spawning)}. Skipping spawning for {protein} and {ligand}.')
+                continue
+
             # Get epochs' folders list
             epochs = []
-            for epoch in os.listdir(pele+'/'+str(spawning)+'/output/output'):
+            for epoch in os.listdir(pele_path+'/'+str(spawning)+'/output/output'):
                 try:
                     epochs.append(int(epoch))
                 except:
                     continue
+
+            if skip_0_epochs:
+                epochs = [e for e in epochs if e !=0]
             epochs = sorted(epochs)
+
+            if not epochs:
+                print(f'There are no epochs for spawning {str(spawning)}. Skipping spawning for {protein} and {ligand}.')
+                continue
 
             for epoch in epochs:
                 current_epoch += 1
-                epoch_folder = pele+'/'+str(spawning)+'/output/output/'+str(epoch)
+                epoch_folder = pele_path+'/'+str(spawning)+'/output/output/'+str(epoch)
                 shutil.copytree(epoch_folder, tmp+'/output/output/'+str(current_epoch))
                 epoch_spawning[current_epoch] = spawning
 
@@ -95,5 +147,6 @@ def formatPELESpawnings(pele_folder, separator='-'):
         with open(tmp+'/._spawning_mapping.json', 'w') as jf:
             json.dump(epoch_spawning, jf)
 
-        shutil.rmtree(pele)
-        shutil.move(tmp, pele)
+        if not keep_original:
+            shutil.rmtree(pele_path)
+            shutil.move(tmp, pele_path)
